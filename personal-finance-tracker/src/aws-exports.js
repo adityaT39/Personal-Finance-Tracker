9ac79bc3b6864071ed23889cc80392f8f5badbd9
Sambhav Garg <78362105+sam131102@users.noreply.github.com>
2024-07-29 12:55:12 -0700

    updated aws-exports with google outh

--- personal-finance-tracker/src/aws-exports.js
+++ personal-finance-tracker/src/aws-exports.js
@@ -1,16 +1,18 @@
 const awsconfig = {
-    aws_project_region: 'ca-central-1',
-    aws_cognito_region: 'ca-central-1',
-    aws_user_pools_id: 'ca-central-1_S9zXbBNeU',
-    aws_user_pools_web_client_id: '5vv9lam4bsfdfnenj2il39uglg',
-    oauth: {
-      domain: 'https://saveit.auth.ca-central-1.amazoncognito.com',
-      scope: ['email', 'profile', 'openid'],
-      redirectSignIn: 'http://localhost:3000/',
-      redirectSignOut: 'http://localhost:3000/',
-      responseType: 'code'
+  aws_project_region: 'ca-central-1',
+  aws_cognito_region: 'ca-central-1',
+  aws_user_pools_id: 'ca-central-1_S9zXbBNeU',
+  aws_user_pools_web_client_id: '5vv9lam4bsfdfnenj2il39uglg',
+  oauth: {
+    domain: 'saveit.auth.ca-central-1.amazoncognito.com',
+    scope: ['email', 'profile', 'openid', 'userinfo.email', 'userinfo.profile', 'user.addresses.read', 'user.birthday.read'],
+    redirectSignIn: 'http://localhost:3000/',
+    redirectSignOut: 'http://localhost:3000/',
+    responseType: 'code',
+    options: {
+      googleClientId: '25082539421-noeujoh2u3p6rsrtp76rkbk5rrrf89no.apps.googleusercontent.com', 
     }
-  };
-  
-  export default awsconfig;
-  
+  }
+};
+
+export default awsconfig;
